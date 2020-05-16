13b020687f4da0ad728c00e0fe484a0ecb5547b0
martinKindall <mart256@gmail.com>
2020-05-16 15:16:05 -0400

    Arranged test inside describe scope.

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,15 +1,29 @@
 import HangmanRules from "./HangmanRules";
 import HangmanGame from "./HangmanGame";
 
-test('Game initialization rules', () => {
+describe('HangmanGame tests', () => {
+  let game: HangmanGame;
   const secretWord = "paralelepipedo";
-  const game = new HangmanGame(secretWord);
 
-  expect(game.livesRemaining).toBe(HangmanRules.maxLives);
-  expect(game.currentWordState.length).toBe(secretWord.length);
+  beforeEach(() => {
+    setupGame();
+  });
 
-  let idx;
-  for (idx = 0; idx < secretWord.length; idx++) {
-    expect(game.currentWordState[idx]).toBeUndefined();
+  function setupGame() {
+    game = new HangmanGame(secretWord);
   }
+
+  test('Game initialization rules', () => {
+    expect(game.livesRemaining).toBe(HangmanRules.maxLives);
+    expect(game.currentWordState.length).toBe(secretWord.length);
+
+    let idx;
+    for (idx = 0; idx < secretWord.length; idx++) {
+      expect(game.currentWordState[idx]).toBeUndefined();
+    }
+  });
+
+  test('Character is not in secret word', () => {
+
+  });
 });
