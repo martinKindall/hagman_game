53bc38a646377449bf26a7f4e873733c3f5f096f
martinKindall <mart256@gmail.com>
2020-05-16 19:53:26 -0400

    Added character input.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,18 @@ import RandomWords from "./RandomWords";
 function App() {
   const game = new HangmanGame(Utils.randomElementFromArray(RandomWords.words));
   const [gameState, setGameState] = useState<string[]>(game.currentWordState);
+  const [character, setCharacter] = useState<string>("");
+
+  const handleCharacterInput = (event: any) => {
+    console.log(event.target.value);
+  };
 
   return (
     <div>
+      <input type={"text"} placeholder={"Ingresa una letra"}
+             value={character}
+             onChange={handleCharacterInput}
+      />
     </div>
   );
 }
