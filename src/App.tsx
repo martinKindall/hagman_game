a60a7308677953306836b1e6105530c5a704fd48
martinKindall <mart256@gmail.com>
2020-05-16 21:42:57 -0400

    Extracted logic to its own component, input char.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import HangmanGame from "./gameController/HangmanGame";
 import Utils from "./Utils";
 import RandomWords from "./RandomWords";
 import {createStyles, Grid, Theme} from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import FormControl from "@material-ui/core/FormControl";
+import InputCharacter from "./components/InputCharacter";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -35,20 +33,9 @@ const game = new HangmanGame(Utils.randomElementFromArray(RandomWords.words));
 function App() {
   const classes = useStyles();
   const [gameState, setGameState] = useState<string[]>(game.currentWordState);
-  const [character, setCharacter] = useState<string>("");
 
-  const handleCharacterInput = (event: any) => {
-    const character = event.target.value;
-    if (character.length > 1) {
-      setCharacter(character[character.length-1]);
-    } else {
-      setCharacter(character);
-    }
-  };
-
-  const handleGuess = () => {
+  const handleOnCharInput = (character: string) => {
     game.guessNextChar(character);
-    setCharacter("");
     setGameState(game.currentWordState);
     console.log(gameState);
   };
@@ -63,23 +50,7 @@ function App() {
             </Toolbar>
         </AppBar>
         <Grid container alignContent={"center"} alignItems={"center"} item spacing={1}>
-          <Grid item xs={2}/>
-          <Grid item xs={8}>
-            <Grid item xs={12} className={classes.inputElem}>
-              <FormControl fullWidth>
-                <TextField id="outlined-basic" label="Siguiente letra"
-                           variant="outlined"
-                           value={character}
-                           onChange={handleCharacterInput}/>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} className={classes.buttonChar}>
-              <Button onClick={handleGuess} variant="contained">
-                Adivinar
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid item xs={2}/>
+          <InputCharacter handleOnCharInput={handleOnCharInput}/>
         </Grid>
       </div>
   );
---
+++ src/components/InputCharacter.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import {createStyles, Grid, Theme} from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    inputElem: {
+      textAlign: "center",
+      marginTop: "1rem"
+    },
+    buttonChar: {
+      textAlign: "right",
+      marginTop: "1rem"
+    }
+  }));
+
+interface Props {
+  handleOnCharInput: (character: string) => void;
+}
+
+const InputCharacter: React.FC<Props> = ({handleOnCharInput}) => {
+  const classes = useStyles();
+  const [character, setCharacter] = useState<string>("");
+
+  const handleCharacterInputOnChange = (event: any) => {
+    const character = event.target.value;
+    if (character.length > 1) {
+      setCharacter(character[character.length-1]);
+    } else {
+      setCharacter(character);
+    }
+  };
+
+  const handleGuess = () => {
+    setCharacter("");
+    handleOnCharInput(character);
+  };
+
+  return <>
+    <Grid item xs={2}/>
+    <Grid item xs={8}>
+      <Grid item xs={12} className={classes.inputElem}>
+        <FormControl fullWidth>
+          <TextField id="outlined-basic" label="Siguiente letra"
+                     variant="outlined"
+                     value={character}
+                     onChange={handleCharacterInputOnChange}/>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} className={classes.buttonChar}>
+        <Button onClick={handleGuess} variant="contained">
+          Adivinar
+        </Button>
+      </Grid>
+    </Grid>
+    <Grid item xs={2}/></>;
+};
+
+export default InputCharacter;
