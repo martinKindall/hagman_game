2a74ad3fa67911e6f38d00159b7749f795950f62
martinKindall <mart256@gmail.com>
2020-05-16 20:07:28 -0400

    Added material input and also input receives 1 character.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,43 @@ import React, {useState} from 'react';
 import HangmanGame from "./gameController/HangmanGame";
 import Utils from "./Utils";
 import RandomWords from "./RandomWords";
+import {createStyles, Grid, Theme} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: "center",
+      marginTop: "1rem"
+    }
+  }));
 
 function App() {
+  const classes = useStyles();
   const game = new HangmanGame(Utils.randomElementFromArray(RandomWords.words));
   const [gameState, setGameState] = useState<string[]>(game.currentWordState);
   const [character, setCharacter] = useState<string>("");
 
   const handleCharacterInput = (event: any) => {
-    console.log(event.target.value);
+    const character = event.target.value;
+    if (character.length > 1) {
+      setCharacter(character[character.length-1]);
+    } else {
+      setCharacter(character);
+    }
   };
 
   return (
-    <div>
-      <input type={"text"} placeholder={"Ingresa una letra"}
-             value={character}
-             onChange={handleCharacterInput}
-      />
-    </div>
+    <Grid container alignContent={"center"} alignItems={"center"} item spacing={1}
+          className={classes.root}>
+      <Grid item xs={12}>
+        <TextField id="outlined-basic" label="Siguiente letra"
+                   variant="outlined"
+                   value={character}
+                   onChange={handleCharacterInput}/>
+      </Grid>
+    </Grid>
   );
 }
 
