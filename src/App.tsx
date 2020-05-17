59777f34476ad747ad6fb454ca82f49826b1edc3
martinKindall <mart256@gmail.com>
2020-05-16 20:11:41 -0400

    Added appbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,19 @@ import RandomWords from "./RandomWords";
 import {createStyles, Grid, Theme} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+    inputElem: {
       textAlign: "center",
       marginTop: "1rem"
     }
@@ -30,15 +39,24 @@ function App() {
   };
 
   return (
-    <Grid container alignContent={"center"} alignItems={"center"} item spacing={1}
-          className={classes.root}>
-      <Grid item xs={12}>
-        <TextField id="outlined-basic" label="Siguiente letra"
-                   variant="outlined"
-                   value={character}
-                   onChange={handleCharacterInput}/>
-      </Grid>
-    </Grid>
+      <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                    Juego del Ahorcado
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <Grid container alignContent={"center"} alignItems={"center"} item spacing={1}
+              className={classes.inputElem}>
+          <Grid item xs={12}>
+            <TextField id="outlined-basic" label="Siguiente letra"
+                       variant="outlined"
+                       value={character}
+                       onChange={handleCharacterInput}/>
+          </Grid>
+        </Grid>
+      </div>
   );
 }
 
