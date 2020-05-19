764bd462ee67ca695507e91e48f8d310f99c5f1c
martinKindall <mart256@gmail.com>
2020-05-19 19:19:09 -0400

    HiddenWord: checking that hidden word is empty.

--- src/tests/HiddenWord.test.ts
+++ src/tests/HiddenWord.test.ts
@@ -2,10 +2,21 @@ import HiddenWord from "../logic/HiddenWord";
 
 describe('HiddenWord tests', () => {
   const word = "paralelepipedo";
+  let hiddenWord: HiddenWord;
+
+  beforeEach(() => {
+    hiddenWord = new HiddenWord(word);
+  });
 
   test('Hidden word has same length', () => {
-    const hiddenWord = new HiddenWord(word);
-
     expect(hiddenWord.state.length).toBe(word.length);
+    wordStateIsUndefined();
   });
+
+  function wordStateIsUndefined() {
+    let idx;
+    for (idx = 0; idx < word.length; idx++) {
+      expect(hiddenWord.state[idx]).toBeUndefined();
+    }
+  }
 });
